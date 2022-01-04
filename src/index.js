291938024960a18bd3766acbbe14852420b6aa77
Matrui <yanmatr@gmail.com>
2022-01-04 21:35:29 +0100

    solutin

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function reverse (n) {
+  let nString = n.toString();
+  let newN = '';
+  if (n > 0) {
+     for (let i = 0; i < nString.length; i++) {
+       newN = `${nString[i]}` + newN;
+     }
+    return Number(newN);
+  }else{
+      nString = Math.abs(nString).toString();
+      for (let i = 0; i < nString.length; i++) {
+        newN = `${nString[i]}` + newN;
+      }
+     return Number(newN);
+  }
   
 }
